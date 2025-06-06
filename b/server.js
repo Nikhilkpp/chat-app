64603e80317ac2b827ad8d9cf744a1be35b1367f
Nikhilkpp <nikhilpathak781@gmail.com>
2025-06-07 02:12:57 +0530

    update package.json

--- b/server.js
+++ b/server.js
@@ -5,7 +5,7 @@ import connectToMongo from "./db/connectToMongo.js";
 import messageRouter from "./routes/message.routes.js";
 import userRouter from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
-
+import path from 'path';
 import cors from 'cors'
 import { app, server } from "./socket/socket.js";
 
@@ -13,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 dotenv.config()
 
 const PORT=process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 // const app=express();    socket se aarha
 
@@ -37,3 +39,8 @@ server.listen(PORT,()=> {
 app.use("/api/auth",authRouter)
 app.use("/api/messages", messageRouter)
 app.use("/api/users", userRouter)
+app.use(express.static(path.join(__dirname, "/f/dist")));
+
+app.get("", (req,res)=>{
+    res.sendFile(path.join(__dirname, "f", "diat", "index.html"))
+})
