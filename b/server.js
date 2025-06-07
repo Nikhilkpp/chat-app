52b552078204ceae4bfbb956db25481bf38b4dbc
Nikhilkpp <nikhilpathak781@gmail.com>
2025-06-07 15:09:07 +0530

    sm

--- b/server.js
+++ b/server.js
@@ -1,31 +1,76 @@
-import express from "express";
-import dotenv from "dotenv";
-import authRouter from "./routes/auth.routes.js";
-import connectToMongo from "./db/connectToMongo.js";
-import messageRouter from "./routes/message.routes.js";
-import userRouter from "./routes/user.routes.js";
-import cookieParser from "cookie-parser";
-import path from 'path';
-import cors from 'cors'
-import { initSocket } from "./socket/socket.js";
-import http from 'http';
+// import express from "express";
+// import dotenv from "dotenv";
+// import authRouter from "./routes/auth.routes.js";
+// import connectToMongo from "./db/connectToMongo.js";
+// import messageRouter from "./routes/message.routes.js";
+// import userRouter from "./routes/user.routes.js";
+// import cookieParser from "cookie-parser";
+// import path from 'path';
+// import cors from 'cors'
+// import { initSocket } from "./socket/socket.js";
+// import http from 'http';
 
 
-dotenv.config()
+// dotenv.config()
 
-const PORT=process.env.PORT || 5000;
+// const PORT=process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-const app=express();   // socket se aarha
-const server = http.createServer(app);
-initSocket(server)
-// app.get('/', (req,res)=>{
-//     res.send("Hello world again!")
+// const app=express();   // socket se aarha
+// const server = http.createServer(app);
+// initSocket(server)
+// // app.get('/', (req,res)=>{
+// //     res.send("Hello world again!")
+// // })
+
+// app.use(express.json())
+// app.use(cookieParser())
+
+// app.use(cors({
+//     origin:process.env.CORS_ORIGIN,
+
+//     credentials:true
+// }))
+
+
+// app.use("/api/auth",authRouter)
+// app.use("/api/messages", messageRouter)
+// app.use("/api/users", userRouter)
+// app.use(express.static(path.join(__dirname, "/f/dist")));
+
+// app.get("*", (req,res)=>{
+//     res.sendFile(path.join(__dirname, "f", "dist", "index.html"))
 // })
 
-app.use(express.json())
-app.use(cookieParser())
+// server.listen(PORT,()=> {
+//     connectToMongo();
+//     console.log("Server is listening",PORT);
+// });
+
+
+
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+dotenv.config();
+
+const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
 
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
@@ -33,17 +78,17 @@ app.use(cors({
     credentials:true
 }))
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.use("/api/auth",authRouter)
-app.use("/api/messages", messageRouter)
-app.use("/api/users", userRouter)
-app.use(express.static(path.join(__dirname, "/f/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, "f", "dist", "index.html"))
-})
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
-server.listen(PORT,()=> {
-    connectToMongo();
-    console.log("Server is listening",PORT);
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
--- b/socket/socket.js
+++ b/socket/socket.js
@@ -1,59 +1,100 @@
-import {Server} from 'socket.io';
-import http from 'http';
-import express from 'express';
+// import {Server} from 'socket.io';
+// import http from 'http';
+// import express from 'express';
 
 
-// const app = express();
+// // const app = express();
 
-let io;
+// let io;
 
-// const server = http.createServer(app)
-// const io = new Server(server, {
-    // cors:{
-    //     origin: ["https://chat-app-ep85.onrender.com"],
-    //     methods: ["GET", "POST"]
-    // }
-// })
-const userSocketMap = {}
+// // const server = http.createServer(app)
+// // const io = new Server(server, {
+//     // cors:{
+//     //     origin: ["https://chat-app-ep85.onrender.com"],
+//     //     methods: ["GET", "POST"]
+//     // }
+// // })
+// const userSocketMap = {}
 
-const initSocket = (server) =>{
-    io = new Server(server, {
-        cors:{
-            origin: ["http://localhost:5173"],
-            methods: ["GET", "POST"]
-        },
-    })
-    io.on('connection',(socket)=>{
-        console.log("a user connected: ",socket.id)
+// const initSocket = (server) =>{
+//     io = new Server(server, {
+//         cors:{
+//             origin: ["http://localhost:5173"],
+//             methods: ["GET", "POST"]
+//         },
+//     })
+//     io.on('connection',(socket)=>{
+//         console.log("a user connected: ",socket.id)
     
-        const userId = socket.handshake.query.userId;
-        if (userId != "undefined") userSocketMap[userId] = socket.id
+//         const userId = socket.handshake.query.userId;
+//         if (userId != "undefined") userSocketMap[userId] = socket.id
     
-        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+//         io.emit("getOnlineUsers", Object.keys(userSocketMap));
     
-        socket.on('disconnect',()=>{
-            console.log("user disconnected", socket.id)
-            delete userSocketMap[userId]
-            io.emit("getOnlineUsers", Object.keys(userSocketMap));
+//         socket.on('disconnect',()=>{
+//             console.log("user disconnected", socket.id)
+//             delete userSocketMap[userId]
+//             io.emit("getOnlineUsers", Object.keys(userSocketMap));
     
-        })
+//         })
     
-    })
-}
+//     })
+// }
 
 
 
 
 
 
- //stores as {userId: socketId}
+//  //stores as {userId: socketId}
 
+
+// export const getReceiverSocketId = (receiverId) => {
+//     return userSocketMap[receiverId];
+
+// }
+
+// // export {app, io, sever}
+// export {initSocket, io}
+
+
+
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+	cors: {
+		origin: ["http://localhost:5173"],
+		methods: ["GET", "POST"],
+	},
+});
 
 export const getReceiverSocketId = (receiverId) => {
-    return userSocketMap[receiverId];
+	return userSocketMap[receiverId];
+};
 
-}
+const userSocketMap = {}; // {userId: socketId}
 
-// export {app, io, sever}
-export {initSocket, io}
+io.on("connection", (socket) => {
+	console.log("a user connected", socket.id);
+
+	const userId = socket.handshake.query.userId;
+	if (userId != "undefined") userSocketMap[userId] = socket.id;
+
+	// io.emit() is used to send events to all the connected clients
+	io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+	// socket.on() is used to listen to the events. can be used both on client and server side
+	socket.on("disconnect", () => {
+		console.log("user disconnected", socket.id);
+		delete userSocketMap[userId];
+		io.emit("getOnlineUsers", Object.keys(userSocketMap));
+	});
+});
+
+export { app, io, server };
 
